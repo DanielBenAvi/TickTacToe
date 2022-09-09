a1fed7baea8bf0090143d6973eb6d8edad798f24
daniel ben avi <daniel.ben.avi10@gmail.com>
2022-09-09 17:34:09 +0300

    added minimax algo to opponent

--- main.py
+++ main.py
@@ -102,6 +102,130 @@ def isMovesLeft(b):
     return False
 
 
+def evaluate(b):
+    # Checking for Rows for X or O victory.
+    for row in range(3):
+        if b[row][0] == b[row][1] and b[row][1] == b[row][2]:
+            if b[row][0] == PLAYER:
+                return 10
+            elif b[row][0] == OPPONENT:
+                return -10
+
+    # Checking for Columns for X or O victory.
+    for col in range(3):
+        if b[0][col] == b[1][col] and b[1][col] == b[2][col]:
+            if b[0][col] == PLAYER:
+                return 10
+            elif b[0][col] == OPPONENT:
+                return -10
+
+    # Checking for Diagonals for X or O victory.
+    if b[0][0] == b[1][1] and b[1][1] == b[2][2]:
+        if b[0][0] == PLAYER:
+            return 10
+        elif b[0][0] == OPPONENT:
+            return -10
+
+    if b[0][2] == b[1][1] and b[1][1] == b[2][0]:
+        if b[0][2] == PLAYER:
+            return 10
+        elif b[0][2] == OPPONENT:
+            return -10
+
+    # Else if none of them have won then return 0
+    return 0
+
+
+def minimax(b, depth, is_max):
+    score = evaluate(b)
+    # If Maximizer has won the game return his/her
+    # evaluated score
+    if score == 10:
+        return score
+
+    # If Minimizer has won the game return his/her
+    # evaluated score
+    if score == -10:
+        return score
+
+    # If there are no more moves and no winner then
+    # it is a tie
+    if not isMovesLeft(b):
+        return 0
+
+    # If this maximizer's move
+    if is_max:
+        best = -1000
+
+        # Traverse all cells
+        for i in range(3):
+            for j in range(3):
+
+                # Check if cell is empty
+                if b[i][j] == BLANK:
+                    # Make the move
+                    b[i][j] = PLAYER
+                    # Call minimax recursively and choose
+                    # the maximum value
+                    best = max(best, minimax(b, depth + 1, not is_max))
+
+                    # Undo the move
+                    b[i][j] = BLANK
+        return best
+
+    # If this minimizer's move
+    else:
+        best = 1000
+        # Traverse all cells
+        for i in range(3):
+            for j in range(3):
+
+                # Check if cell is empty
+                if b[i][j] == BLANK:
+                    # Make the move
+                    b[i][j] = OPPONENT
+
+                    # Call minimax recursively and choose
+                    # the minimum value
+                    best = min(best, minimax(b, depth + 1, not is_max))
+
+                    # Undo the move
+                    b[i][j] = BLANK
+        return best
+
+
+def findBestMove(b):
+    best_val = -1000
+    bm = (-1, -1)
+
+    # Traverse all cells, evaluate minimax function for
+    # all empty cells. And return the cell with optimal
+    # value.
+    for i in range(3):
+        for j in range(3):
+
+            # Check if cell is empty
+            if b[i][j] == BLANK:
+
+                # Make the move
+                board[i][j] = PLAYER
+
+                # compute evaluation function for this
+                # move.
+                move_val = minimax(board, 0, False)
+
+                # Undo the move
+                board[i][j] = BLANK
+
+                # If the value of the current move is
+                # more than the best value, then update
+                # best/
+                if move_val > best_val:
+                    bm = (i, j)
+                    best_val = move_val
+    return bm
+
+
 """
     game variables
 """
@@ -142,7 +266,6 @@ while True:
         break
 
     if event in ['-reset-']:
-        turn = change_player(turn)
         window['-player-'].update(turn)
         board = reset_board(board)
         reset_gui(window, board)
@@ -151,57 +274,54 @@ while True:
     if event in ['-00-']:
         if board[0][0] == BLANK and not game_over:
             board[0][0] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-01-']:
         if board[0][1] == BLANK and not game_over:
             board[0][1] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-02-']:
         if board[0][2] == BLANK and not game_over:
             board[0][2] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-10-']:
         if board[1][0] == BLANK and not game_over:
             board[1][0] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-11-']:
         if board[1][1] == BLANK and not game_over:
             board[1][1] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-12-']:
         if board[1][2] == BLANK and not game_over:
             board[1][2] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-20-']:
         if board[2][0] == BLANK and not game_over:
             board[2][0] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-21-']:
         if board[2][1] == BLANK and not game_over:
             board[2][1] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
 
     if event in ['-22-']:
         if board[2][2] == BLANK and not game_over:
             board[2][2] = turn
-            turn = change_player(turn)
             change_board(window, board, turn)
-
+    """
+        opponent turn
+    """
+    if isMovesLeft(board):
+        best_move = findBestMove(board)
+        board[best_move[0]][best_move[1]] = OPPONENT
+        change_board(window, board, turn)
     """
         check if game over
     """
