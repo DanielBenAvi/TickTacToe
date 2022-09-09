608a4abbab17e1e065bf5c0758c206384ea93b78
daniel ben avi <daniel.ben.avi10@gmail.com>
2022-09-09 17:11:44 +0300

    changed the game to player and opponent

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import PySimpleGUI as Gui
 
 """
 Gui.theme('DarkBlue2')
-PLAYER_OPTIONS = ['X', 'O']
+PLAYER = 'X'
+OPPONENT = 'O'
 PLAYER_FONT = 'Ariel 20'
 MAIN_FONT = 'Ariel 24'
 CELL_FONT = 'Stencil 60'
@@ -44,10 +45,10 @@ def change_player(p):
         return 'X'
 
 
-def check_rows(w, b, options):
+def check_rows(w, b, player, opponent):
     for i in range(0, 3):
-        if (b[i][0] == options[0] and b[i][1] == options[0] and b[i][2] == options[0]) or \
-                (b[i][0] == options[1] and b[i][1] == options[1] and b[i][2] == options[1]):
+        if (b[i][0] == player and b[i][1] == player and b[i][2] == player) or \
+                (b[i][0] == opponent and b[i][1] == opponent and b[i][2] == opponent):
             w[f'-{i}0-'].update(text_color='red')
             w[f'-{i}1-'].update(text_color='red')
             w[f'-{i}2-'].update(text_color='red')
@@ -55,10 +56,10 @@ def check_rows(w, b, options):
     return False
 
 
-def check_cols(w, b, options):
+def check_cols(w, b, player, opponent):
     for i in range(0, 3):
-        if (b[0][i] == options[0] and b[1][i] == options[0] and b[2][i] == options[0]) or \
-                (b[0][i] == options[1] and b[1][i] == options[1] and b[2][i] == options[1]):
+        if (b[0][i] == player and b[1][i] == player and b[2][i] == player) or \
+                (b[0][i] == opponent and b[1][i] == opponent and b[2][i] == opponent):
             w[f'-0{i}-'].update(text_color='red')
             w[f'-1{i}-'].update(text_color='red')
             w[f'-2{i}-'].update(text_color='red')
@@ -66,9 +67,9 @@ def check_cols(w, b, options):
     return False
 
 
-def check_left_dig(w, b, options):
-    if (b[0][0] == options[0] and b[1][1] == options[0] and b[2][2] == options[0]) or \
-            (b[0][0] == options[1] and b[1][1] == options[1] and b[2][2] == options[1]):
+def check_left_dig(w, b, player, opponent):
+    if (b[0][0] == player and b[1][1] == player and b[2][2] == player) or \
+            (b[0][0] == opponent and b[1][1] == opponent and b[2][2] == opponent):
         w[f'-00-'].update(text_color='red')
         w[f'-11-'].update(text_color='red')
         w[f'-22-'].update(text_color='red')
@@ -76,9 +77,9 @@ def check_left_dig(w, b, options):
     return False
 
 
-def check_right_dig(w, b, options):
-    if (b[0][2] == options[0] and b[1][1] == options[0] and b[2][0] == options[0]) or (
-            b[0][2] == options[1] and b[1][1] == options[1] and b[2][0] == options[1]):
+def check_right_dig(w, b, player, opponent):
+    if (b[0][2] == player and b[1][1] == player and b[2][0] == player) or (
+            b[0][2] == opponent and b[1][1] == opponent and b[2][0] == opponent):
         w[f'-02-'].update(text_color='red')
         w[f'-11-'].update(text_color='red')
         w[f'-20-'].update(text_color='red')
@@ -87,10 +88,10 @@ def check_right_dig(w, b, options):
 
 
 def if_game_over():
-    return check_rows(window, board, PLAYER_OPTIONS) or \
-           check_cols(window, board, PLAYER_OPTIONS) or \
-           check_left_dig(window, board, PLAYER_OPTIONS) or \
-           check_right_dig(window, board, PLAYER_OPTIONS)
+    return check_rows(window, board, PLAYER, OPPONENT) or \
+           check_cols(window, board, PLAYER, OPPONENT) or \
+           check_left_dig(window, board, PLAYER, OPPONENT) or \
+           check_right_dig(window, board, PLAYER, OPPONENT)
 
 
 def isMovesLeft(b):
@@ -105,14 +106,14 @@ def isMovesLeft(b):
     game variables
 """
 game_over = False
-player = PLAYER_OPTIONS[0]
+turn = PLAYER
 board = [[BLANK, BLANK, BLANK],
          [BLANK, BLANK, BLANK],
          [BLANK, BLANK, BLANK]]
 
 layout = [[Gui.Text('Tic-Tac-Toe', expand_x=True, font=MAIN_FONT, key='-mainText-', justification='center')],
           [Gui.Text('Turn:', font=PLAYER_FONT, expand_x=True),
-           Gui.Text(text=player, key='-player-', font=PLAYER_FONT, expand_x=True, justification='right')],
+           Gui.Text(text=turn, key='-player-', font=PLAYER_FONT, expand_x=True, justification='right')],
           [Gui.Button('Reset', expand_x=True, key='-reset-')],
           [Gui.Text('', auto_size_text=True, size=(CELL_WIDTH, CELL_HEIGHT), key='-00-', relief=Gui.RELIEF_SOLID,
                     border_width=2, justification='center', font='Stencil 60', enable_events=True),
@@ -141,71 +142,71 @@ while True:
         break
 
     if event in ['-reset-']:
-        player = change_player(player)
-        window['-player-'].update(player)
+        turn = change_player(turn)
+        window['-player-'].update(turn)
         board = reset_board(board)
         reset_gui(window, board)
         game_over = False
 
     if event in ['-00-']:
         if board[0][0] == BLANK and not game_over:
-            board[0][0] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[0][0] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-01-']:
         if board[0][1] == BLANK and not game_over:
-            board[0][1] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[0][1] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-02-']:
         if board[0][2] == BLANK and not game_over:
-            board[0][2] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[0][2] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-10-']:
         if board[1][0] == BLANK and not game_over:
-            board[1][0] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[1][0] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-11-']:
         if board[1][1] == BLANK and not game_over:
-            board[1][1] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[1][1] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-12-']:
         if board[1][2] == BLANK and not game_over:
-            board[1][2] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[1][2] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-20-']:
         if board[2][0] == BLANK and not game_over:
-            board[2][0] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[2][0] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-21-']:
         if board[2][1] == BLANK and not game_over:
-            board[2][1] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[2][1] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     if event in ['-22-']:
         if board[2][2] == BLANK and not game_over:
-            board[2][2] = player
-            player = change_player(player)
-            change_board(window, board, player)
+            board[2][2] = turn
+            turn = change_player(turn)
+            change_board(window, board, turn)
 
     """
         check if game over
     """
     if if_game_over():
         game_over = True
-        window['-player-'].update(f'GAME OVER, {change_player(player)} wins')
+        window['-player-'].update(f'GAME OVER, {change_player(turn)} wins')
 
 window.close()
